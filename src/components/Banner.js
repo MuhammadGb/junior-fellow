18d551dbc3cb8a075c1089f7c083467ed152a06f
Hamg69 <muhammad17gb@gmail.com>
2021-07-28 07:06:29 +0000

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,49 @@
 import * as React from 'react';
 import banner from '../assets/banner.jpeg';
 import { Box, Image, Text } from '@chakra-ui/react';
+import '../assets/fonts.css';
 
 export default function Banner() {
   return (
-    <Box boxSize="lg">
-      <Image src={banner} alt="banner" />
-      <Text fontSize="3xl">Watch something incredible.</Text>
+    <Box>
+      <Box 
+        sx={{
+            border: "2px solid red",
+            height: "680px",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center"
+            }}
+        >
+        <Text 
+          sx={{
+              width: "26%",
+              position: "relative",
+              zIndex: 1,
+              left: 80,
+              top: 40,
+              fontSize: "76px",
+              letterSpacing: "-3px", 
+              fontWeight: 600,
+              fontFamily: "Dm Sans",
+              border: "2px solid blue",
+              color: "#FFFFFF"
+              }}
+        >Watch something incredible.</Text>
+      </Box>
+      <Box>
+        <Image 
+          src={banner} 
+          alt="banner"
+          sx={{
+              width: "100%",
+              top: "140px",
+              height: "700px",
+              position: "absolute",
+              zIndex: -1,
+              }}
+          />
+      </Box>
     </Box>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,31 @@ import { Box, Heading } from '@chakra-ui/react';
 
 export default function Navbar() {
   return (
-    <Box bg="black" w="90%" p={2} color="white">
-      <Heading size="lg" fontSize="30px">
+    <Box 
+        bg="#292929" 
+        w="100%" 
+        color="#FFFFFF" 
+        sx={{
+            zIndex: 1,
+            position: "absolute",
+            height: "140px",
+            alignItems: "center",
+            }}
+        >
+      <Heading 
+        fontSize="35px" 
+        sx={{
+            position: "absolute",
+            left: "77px",
+            zIndex: 100,
+            top: "10px",
+            padding: "11px 19px",
+            border: "1px solid #FFFFFF",
+            fontFamily: "Merriweather", 
+            letterSpacing: -2,
+            fontWeight: 300,
+            }}
+        >
         MyTestApp
       </Heading>
     </Box>
