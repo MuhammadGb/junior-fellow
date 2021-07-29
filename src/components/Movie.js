b2c449babf8b3a97542627058c864517cd7bc2f7
Hamg69 <muhammad17gb@gmail.com>
2021-07-29 07:16:14 +0000

    commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 export default function Movie() {
 
+    //const fetchMovies = fetch("http://www.omdbapi.com/?apikey=df7a2100&");
 
-    const fetchMovies = fetch("http://www.omdbapi.com/?apikey=df7a2100&");
-
-    const fetchMovies = fetch("http://www.omdbapi.com/?i=tt3896198&apikey=df7a2100")
+    const fetchMovies = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=df7a2100")
+        .then(res => {await res.json()});
 }
