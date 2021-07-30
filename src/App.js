e454d8f7325294c74e0cd161fcf3d3b4c9fa772f
Hamg69 <muhammad17gb@gmail.com>
2021-07-30 06:52:23 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Movie from './components/Movie';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Movie />
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,8 +2,23 @@ import React, {useState, useEffect} from 'react';
 
 export default function Movie() {
 
+    const [movies, setMovies] = useState([]);
+
     //const fetchMovies = fetch("http://www.omdbapi.com/?apikey=df7a2100&");
 
-    const fetchMovies = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=df7a2100")
-        .then(res => {await res.json()});
+    const fetchMovies = () => {
+        fetch("http://www.omdbapi.com/?s=batman&page=2&apikey=df7a2100")
+            .then(res => res.json())
+            .then(res =>  {
+                setMovies(res.Search);
+                console.log(res.Search);
+            });
+    };
+    useEffect(() => {
+        fetchMovies();
+    }, [])
+
+    return(
+        <div></div>
+    )
 }
